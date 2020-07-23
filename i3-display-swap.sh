3ad9ad07c67d2b2354488be14d28a3fd71767836
Jasper Spahl <jasperspahl@web.de>
2020-07-24 00:23:20 +0200

    Fixed a mistake in i3-display-swap.sh

--- i3-display-swap.sh
+++ i3-display-swap.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # requires jq
 
-DISPLAY_CONFIG=($(i3-msg -t get_outputs | jq -r '.[]|"\(.name):\(current_workspace)"'))
+DISPLAY_CONFIG=($(i3-msg -t get_outputs | jq -r '.[]|"\(.name)=\(.current_workspace)"'))
 
 for ROW in "${DISPLAY_CONFIG[@]}"
 do
-	IFS=':'
+	IFS='='
 	read -ra CONFIG <<< "${ROW}"
 	if [ "${CONFIG[0]}" != "null" ] && [ "${CONFIG[1]}" != "null" ]; then
 		echo "moving ${CONFIG[1]} right ..."
